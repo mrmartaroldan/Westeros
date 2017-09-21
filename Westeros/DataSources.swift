6822fc5b934187764b4ddeeb082e0b2efad71ffe
Marta Roldan <mrmartaroldan@gmail.com>
2017-09-21 16:24:28 +0100

    Create generic Delegate

--- Westeros/DataSources.swift
+++ Westeros/DataSources.swift
@@ -3,22 +3,20 @@
 //  Westeros
 //
 //  Created by Marta Roldán Arellano on 21/9/17.
-//  Copyright © 2017 Keepcoding. All rights reserved.
+//  Copyright © 2017 Marta Roldán Arellano. All rights reserved.
 //
 
 import UIKit
 
 // Dos funciones que devuelven las DataSources que necesitamos: Houses & Persons
 // La clase DataSources existe simplemente para agrupar dichas funciones
-final class DataSources{
+final class DataSource{
     static func housesDataSource(model: [House]) -> ArrayDataSource<House>{
-        return ArrayDataSource(model: model) { (house:House, tableView:
-            UITableView) -> UITableViewCell in
+        return ArrayDataSource(model: model) { (house:House, tableView: UITableView) -> UITableViewCell in
             let cellID = "House"
             var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
             if cell == nil{
-                cell = UITableViewCell(style: .subtitle, reuseIdentifier:
-                    cellID)
+                cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellID)
             }
             cell?.imageView?.image = house.sigil.image
             cell?.textLabel?.text = house.name
@@ -27,13 +25,11 @@ final class DataSources{
         }
     }
     static func personsDataSource(model: [Person]) -> ArrayDataSource<Person>{
-        return ArrayDataSource(model: model) { (person:Person, tableView:
-            UITableView) -> UITableViewCell in
+        return ArrayDataSource(model: model) { (person:Person, tableView: UITableView) -> UITableViewCell in
             let cellID = "Person"
             var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
             if cell == nil{
-                cell = UITableViewCell(style: .default, reuseIdentifier:
-                    cellID)
+                cell = UITableViewCell(style: .default, reuseIdentifier: cellID)
             }
             cell?.textLabel?.text = person.fullName
             return cell!
---
+++ Westeros/Delegates.swift
@@ -0,0 +1,27 @@
+//
+//  Delegate.swift
+//  Westeros
+//
+//  Created by Marta Roldán Arellano on 21/9/17.
+//  Copyright © 2017 Marta Roldán Arellano. All rights reserved.
+//
+
+import UIKit
+
+    
+class BaseViewControllerDelegate<Element>: NSObject {
+    var source: ArrayDataSource<Element>?
+    var viewController: UIViewController?
+}
+final class GreatHousesDelegate: BaseViewControllerDelegate<House>, UITableViewDelegate {
+        
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let house = source?.element(atIndexPath: indexPath),
+            let nav = viewController?.navigationController {
+            
+            let vc = HouseViewController(model: house)
+            nav.pushViewController(vc, animated: true)
+        }
+    }
+}
+
